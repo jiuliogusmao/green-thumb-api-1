d3e810b5213e9c3a31c54f9648268a3d67f81486
jiuliogusmao <jiuliogusmao@gmail.com>
2023-01-05 16:34:58 -0300

    tipo Boolean para bool e remoção do try/catch

--- App/Model/User.cs
+++ App/Model/User.cs
@@ -6,52 +6,24 @@ public class User
     public string? Password { get; set; }
     public string? Email { get; set; }
 
-    public static Boolean strongPassword(string password)
+    public static bool strongPassword(string password)
     {
-        try {
         if (password.Length < 6 || password.Length > 12)
-            throw new ArgumentException (paramName: nameof(password), message:"The password must be 6 to 12 characters long");
+            return false;
 
-        } catch (ArgumentException e){
-            Console.WriteLine(e);
-        }
-
-        try {
         if (!password.Any(c => char.IsDigit(c)))
-            throw new ArgumentException (paramName: nameof(password), message:"The password must have at least one number");
+            return false;
 
-        } catch (ArgumentException e){
-            Console.WriteLine(e);
-
-        }
-
-        try {
         if (!password.Any(c => char.IsUpper(c)))
-            throw new ArgumentException (paramName: nameof(password), message:"The password must have at least one uppercase character");
+            return false;
 
-        } catch (ArgumentException e){
-            Console.WriteLine(e);
-
-        }
-
-        try {
         if (!password.Any(c => char.IsLower(c)))
-            throw new ArgumentException (paramName: nameof(password), message:"The password must have at least one lowercase character");
+            return false;
 
-        } catch (ArgumentException e){
-            Console.WriteLine(e);
-
-        }
-
-        try {
         if (!password.Any(c => char.IsSymbol(c)))
-            throw new ArgumentException (paramName: nameof(password), message:"The password must have at least one special character");
+            return false;
 
-        } catch (ArgumentException e){
-            Console.WriteLine(e);
-
-        }
-        
+        else
         return true;
     }
 
