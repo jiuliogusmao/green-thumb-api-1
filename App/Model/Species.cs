b60241ba3c341ca1a48e9aa30c50ec8a958ab17e
Bruno Silva <brunosilva.dev@gmail.com>
2023-02-27 14:22:15 -0600

    ScientificName vs CommonName

--- App/Model/Species.cs
+++ App/Model/Species.cs
@@ -7,7 +7,8 @@ public record Species
         Cares = new List<Care>();
     }
     public int SpeciesId { get; set; }
-    public string? Name{get; set;}
+    public string? CommonName { get; set; }
+    public string? ScientificName { get; set; }
 
     public List<Care> Cares { get; set; }
 }
