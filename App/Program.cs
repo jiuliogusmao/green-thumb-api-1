f14f2e182c4094af57d358c711002e863c1ad2c9
Bruno Silva <brunosilva.dev@gmail.com>
2023-03-03 12:13:34 -0600

    added CORS

--- App/Program.cs
+++ App/Program.cs
@@ -2,9 +2,15 @@ using GreenThumb.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDIServices();
+builder.Services.AddCors();
 var app = builder.Build();
 
 app.UseOpenApi();
 app.UseEndpoints();
 
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:4200", "https://gentle-plant-06a96d810.2.azurestaticapps.net")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
 app.Run();
